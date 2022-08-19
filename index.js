887c6a3ebbe8a9f499725c6190b692d59974bb69
seba-sierra <gerardosebastiansierra@gmail.com>
2022-08-18 21:17:25 -0300

    init

---
+++ index.js
@@ -0,0 +1,7 @@
+let respuesta = prompt("No esta vivo, pero aun asi crece.\nNunca respira, pero sin aire... perece.\nAunque tu no lo creas su calor siempre ofrece.\n\nArriesga una respuesta");
+
+while(respuesta != "fuego") {
+    alert("Tu respuesta, " + respuesta + " no es la correcta.");
+    respuesta = prompt("puedes intentarlo de nuevo");
+
+}
